b074cdc082f91e614095a73378d30035abab5397
asimetra404 <asimetra404@gmail.com>
2025-02-25 16:26:30 +0300

    feat: refactor environment allocator and operations for better memory management

--- src/env/operations.c
+++ src/env/operations.c
@@ -1,20 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_operations.c                                   :+:      :+:    :+:   */
+/*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hsamir <hsamir@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 09:39:20 by hsamir            #+#    #+#             */
-/*   Updated: 2025/02/25 10:36:00 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/02/25 16:14:09 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
-#include <stdlib.h>
+#include "libft.h"
+#include "minishell.h"
 
-t_env	*get_env_value(char *key);
+t_env	*get_env(char *key)
+{
+	t_env	*env;
 
-t_env	*set_env_value(char *key, char *value);
+	env = get_root_env();
+	while (env)
+	{
+		if (str_equal(env->key, key))
+			return (env);
+		env = env->next;
+	}
+	return (NULL);
+}
+
+char	*get_env_value(char *key)
+{
+	return (get_env(key)->value);
+}
+
+int	set_env_value(char *key, char *value)
+{
+	t_env	*env;
+	char	*new_val;
+
+	new_val = malloc (size)
+	env = get_env(key);
+	if (env == NULL)
+	{
+		env = create_env(key, ft_strdup(value));
+		if (env)
+	}
+	else
+	{
+		free(env->value);
+		env->value = ft_strdup(value);
+	}
+}
 
 void	init_env(char *envp[]);
