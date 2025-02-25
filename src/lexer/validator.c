e1492698a7754359cbbbf87b454e93c3216297dc
Ahmet Talha Ekinci <ahekinci@k2m07s03.42kocaeli.com.tr>
2025-02-25 16:08:14 +0300

    feat: implement quote syntax validation in token checker and add initial README and TODO files

--- src/lexer/validator.c
+++ src/lexer/validator.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate_tokens.c                                  :+:      :+:    :+:   */
+/*   validator.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsamir <hsamir@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*   By: ahekinci <ahekinci@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:46:06 by hsamir            #+#    #+#             */
-/*   Updated: 2025/02/23 13:03:55 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/02/25 16:03:07 by ahekinci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,27 @@ int check_input_syntax(char* input);
 // if operator token is first or last token, it will be syntax error
 // if operator token is followed by another operator token, it will be syntax error
 int check_tokens_syntax(t_token *token);
+
+
+int	quote_check(char *str)
+{
+	char	quote;
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+		{
+			quote = str[i];
+			i++;
+			while (str[i] && str[i] != quote)
+				i++;
+			if (!str[i])
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
 
