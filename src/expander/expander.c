ad968491d55a7ea6bb353ab781376ff6cb71e9f8
asimetra404 <asimetra404@gmail.com>
2025-03-01 06:15:00 +0300

    docs: update documents about the expander

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,15 +6,21 @@
 /*   By: hsamir <hsamir@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 23:56:45 by hsamir            #+#    #+#             */
-/*   Updated: 2025/02/28 16:40:58 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/03/01 00:02:41 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	2.6.5 Field Splitting {
-		=> https://pubs.opengroup.org/onlinepubs/9699919799.2018edition/utilities/V3_chap02.html#tag_18_06_05		
+		=> https://pubs.opengroup.org/onlinepubs/9699919799.2018edition/utilities/V3_chap02.html#tag_18_06_05
+		=> default IFS value: {
+			<space>,
+			<tab>,
+			<newline>
+		}
 	},
 	2.6.7 Quote Removal {
-		The quote characters ( <backslash>, single-quote, and double-quote) that were present in the original word shall be removed unless they have themselves been quoted.
+		=> The quote characters ( <backslash>, single-quote, and double-quote) that were present in the original word shall be removed unless they have themselves been quoted.
 	}
 */
+
