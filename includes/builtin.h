847b80dac70ac489047073b7dce9758aa9c303d8
simy404 <asimetra404@gmail.com>
2025-05-19 15:09:55 +0200

    refactor:  add  built-in header

--- includes/builtin.h
+++ includes/builtin.h
@@ -1,17 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inbuild.h                                          :+:      :+:    :+:   */
+/*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahekinci <ahekinci@student.42kocaeli.co    +#+  +:+       +#+        */
+/*   By: hsamir <hsamir@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 16:39:04 by ahekinci          #+#    #+#             */
-/*   Updated: 2025/03/02 16:56:06 by ahekinci         ###   ########.fr       */
+/*   Updated: 2025/05/19 15:08:59 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INBUILD_H
-# define INBUILD_H
+#ifndef BUILTIN_H
+# define BUILTIN_H
 
-int echo(char flag, char **params);
+# include "command.h"
+
+int		ft_echo(t_command *cmd);
+int		ft_cd(t_command *cmd);
+int		ft_pwd(t_command *cmd);
+int		ft_export(t_command *cmd);
+int		ft_unset(t_command *cmd);
+int		ft_env(t_command *cmd);
+int		ft_exit(t_command *cmd);
+
+
+typedef int	(*t_builtin)(t_command *cmd);
+
+int		is_builtin(char *cmd);
+int		is_simple_command(t_command *cmd);
+
 #endif
+
