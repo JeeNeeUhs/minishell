9f4ee649068a8d4c4e4336031675f2967b711956
simy404 <asimetra404@gmail.com>
2025-05-20 15:56:10 +0200

    add: unset builtin func

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,22 @@
 /*   By: hsamir <hsamir@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 23:22:41 by hsamir            #+#    #+#             */
-/*   Updated: 2025/05/19 23:22:42 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/05/20 15:46:38 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "command.h"
+
+/* Unsetting a variable or function that was not previously set shall
+     not be considered an error and does not cause the shell to abort.*/
+void	unset_builtin(t_command *command)
+{
+	int	index;
+
+	index = 1;
+	while (command->args[index])
+	{
+		remove_env(command->args[index]);
+		index++;
+	}
+}
